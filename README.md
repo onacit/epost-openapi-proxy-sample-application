85eaa4ee476c87a16fe1f39aed65cc9d2799ce35
Jin Kwon <onacit@gmail.com>
2024-09-28 19:53:13 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# epost-openapi-proxy-sample-application
+
+A sample application runs [epost-openapi-proxy](https://github.om/jinahya/epost-openapi-proxy).
+
+## How to run
+
+```commandline
+$ SERVICE_KEY=<your-own-URL-DECODED-service-key> mvn spring-boot:run
+```
---
+++ src/main/java/com/github/onacit/epost/openapi/proxy/Application.java
@@ -0,0 +1,29 @@
+package com.github.onacit.epost.openapi.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.support.WebStack;
+
+@EnableHypermediaSupport(
+        type = {
+                EnableHypermediaSupport.HypermediaType.HAL
+        },
+        stacks = {
+                WebStack.WEBFLUX
+        }
+)
+@ComponentScan(
+        basePackageClasses = {
+                Application.class,
+                com.github.jinahya.epost.openapi.proxy.NoOp.class
+        }
+)
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
